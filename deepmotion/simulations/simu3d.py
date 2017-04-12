a9d71e30d6ed0d6cba6060ae63d89edc5155e674
cdansereau <christiandansereau@gmail.com>
2017-04-12 17:26:03 -0400

    minor

--- deepmotion/simulations/simu3d.py
+++ deepmotion/simulations/simu3d.py
@@ -5,10 +5,10 @@ from .. import registration
 
 def avgpool3d(hr_vols, k=3):
 
-    with tf.device('/gpu:1'):
+    with tf.device('/gpu:0'):
         x = tf.placeholder(tf.float32, shape=(hr_vols.shape[0], hr_vols.shape[1], hr_vols.shape[2], hr_vols.shape[3], 1))
         y = tf.nn.avg_pool3d(x, ksize=[1, k, k, k, 1], strides=[1, k, k, k, 1], padding='SAME')
-    init = tf.global_variables_initializer()
+        init = tf.global_variables_initializer()
     with tf.Session() as sess:
         results = sess.run(y, feed_dict={x: hr_vols[:, ...][..., np.newaxis]})
 
